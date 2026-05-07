19b872e908de8d8c8a87c925e21fefee0db87a0e
everhaulla <info@everhaulsolutions.com>
2026-05-07 14:29:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
 export default function Home() {
   return (
-    <main style={{
-      background: "#111",
-      color: "white",
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      fontFamily: "Arial"
-    }}>
-      <h1>Everhaul Website Live 🚛</h1>
-      <p>Los Angeles Junk Removal & Hauling</p>
+    <main>
+      <h1>Everhaul Solutions</h1>
+      <p>Los Angeles Junk Removal</p>
     </main>
   )
 }
