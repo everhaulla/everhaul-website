77f384e333b52ed1e0d3ee46ff783756db638a0f
everhaulla <info@everhaulsolutions.com>
2026-05-07 18:06:20 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import Navbar from "../components/Navbar";
+import HomeSections from "../components/HomeSections";
+import Footer from "../components/Footer";
 import Head from "next/head";
 import HeroSection from "../components/HeroSection";
 
