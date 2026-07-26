ba937aff726dcbf9fb638c7080a0e665f401e421
everhaulla <info@everhaulsolutions.com>
2026-07-26 15:40:19 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18352022232"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    import "../styles/globals.css";
+import Script from "next/script";
 
-  gtag('config', 'AW-18352022232');
-</script>
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18352022232"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18352022232');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
