2ec5e6634430e3db4fd0100179dd74eafb48aed9
everhaulla <info@everhaulsolutions.com>
2026-07-30 14:56:53 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,32 @@
 import Script from "next/script";
 import "../styles/globals.css";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18352022232"
-        strategy="afterInteractive"
-      />
-      <Script id="google-ads-tag" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-18352022232');
-        `}
-      </Script>
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18352022232"
+        strategy="afterInteractive"
+      />
 
-      <Component {...pageProps} />
-    </>
-  );
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18352022232');
+        `}
+      </Script>
+
+      <Script id="google-ads-call-tracking" strategy="afterInteractive">
+        {`
+          gtag('config', 'AW-18352022232/2bUGCI3hq9kcENjF9q5E', {
+            phone_conversion_number: '8185381072'
+          });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
